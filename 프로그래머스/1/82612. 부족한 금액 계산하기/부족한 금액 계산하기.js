23cdf1de07b0327955121a0bad62a26623558db8
chocorogit <147039943+chocorogit@users.noreply.github.com>
2023-12-04 09:45:45 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -2,12 +2,9 @@ function solution(price, money, count) {
     let newPrice = 0;
   for(let i=0; i<=count; i++) {
       newPrice += (price*i);
-          
   }
       if(newPrice > money) {
-          console.log("newPrice",newPrice);
-          console.log("money",money);
           return newPrice - money;
       }
-      else {return 0;}
+    return 0;
 }
